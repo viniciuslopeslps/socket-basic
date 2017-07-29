f1b2761bd8d9fdafdf7344414e6c54cf0f02f109
Vinicius Lopes <viniciuslopeslps@gmail.com>
2017-07-28 22:43:36 -0300

    putting socker.io

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ var PORT = process.env.PORT | 3000;
 var express = require("express");
 var app = express();
 var http = require("http").Server(app);
-
+var io = require("socket.io")(socket); //funciona como o app
 
 app.use(express.static(__dirname + "/public"));
+
+// on = permite você escultar por eventos
+io.on("connections", function(){ //esculta o evendo de get connection do client
+	console.log("User connected via socket.io");
+}); 
 
 app.get("/", function(req, res){
 	res.render("index.html");
