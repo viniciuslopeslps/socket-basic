f8209499e65dc04a5ff13cf89ea8651120fe33e9
Vinicius Lopes <viniciuslopeslps@gmail.com>
2017-07-30 18:33:34 -0300

    initial commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var PORT = process.env.PORT | 3000;
 var express = require("express");
 var app = express();
 var http = require("http").Server(app);
-var io = require("socket.io")(socket); //funciona como o app
+var io = require("socket.io")(http); //funciona como o app
 
 app.use(express.static(__dirname + "/public"));
 
