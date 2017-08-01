6b24a0bd9e111c1001628d98390748851d5c4ea7
Vinicius Lopes <viniciuslopeslps@gmail.com>
2017-07-31 21:04:11 -0300

    reading value of input

--- public/js/app.js
+++ public/js/app.js
@@ -7,3 +7,15 @@ socket.on("connect", function(){ //envia o evento de conexao para o server (conn
 socket.on("message", function(message){
     console.log("Receving data in frontend: " + message.text);
 });
+
+
+//recebe evendo de submit de mensagem
+var $form = jQuery("#message-form");
+
+$form.on("submit", function(event){
+	event.preventDefault();
+	var $message = $form.find("input[name=message]");
+	socket.emit("message", {
+		text: $message.val()
+	});
+});
